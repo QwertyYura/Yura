57c959a541422a72ed767f4ee828744c61069446
Yura Shanin <yurashanin@yandex.ru>
2019-10-15 21:34:37 +0300

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+ITMO Java lesson project.
---
+++ src/main/java/com/ifmo/lesson1/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.ifmo.lesson1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, wold!");
+    }
+}
