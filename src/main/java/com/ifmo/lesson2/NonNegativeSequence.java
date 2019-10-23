5d01f6134fb57da21775ee1ecb7e0e1d5af3b048
Yura Shanin <yurashanin@yandex.ru>
2019-10-23 14:25:03 +0300

    NonNegativeSequence

---
+++ src/main/java/com/ifmo/lesson2/NonNegativeSequence.java
@@ -0,0 +1,13 @@
+package com.ifmo.lesson2;
+
+public class NonNegativeSequence {
+    /*
+    Создайте программу, выводящую на экран все неотрицательные элементы
+    последовательности 90 85 80 75 70 65 60 ….
+     */
+    public static void main(String[] args) {
+        for (int i=90; i>0; i=i-5){
+            System.out.print(i+" ");
+        }
+    }
+}
