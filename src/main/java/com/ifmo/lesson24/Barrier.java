14e94c7d011bfb9ca091894b91ce5ddd79f78920
Yura Shanin <yurashanin@yandex.ru>
2019-12-07 20:10:32 +0300

    Control_point

---
+++ src/main/java/com/ifmo/lesson24/Barrier.java
@@ -0,0 +1,23 @@
+package com.ifmo.lesson24;
+
+public class Barrier extends Thread {
+
+    private int countThread;
+
+    public Barrier(int countThread) {
+
+        this.countThread = countThread;
+
+    }
+
+    int i=1;
+    public synchronized void await() throws InterruptedException{
+
+        while (i<=countThread){
+                System.out.println("Пришел поток №"+i);
+                i++;
+                wait();
+            }
+        notifyAll();
+    }
+}
---
+++ src/main/java/com/ifmo/lesson24/Program.java
@@ -0,0 +1,26 @@
+package com.ifmo.lesson24;
+
+public class Program {
+    public static void main(String[] args) throws InterruptedException {
+
+        int c = 3;
+
+        Barrier b = new Barrier(c);
+
+        for (int i = 0; i < c; i++) {
+            new Thread(){
+                @Override
+                public void run() {
+                    System.out.println("Before barrier");
+                    try {
+                        b.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("After barrier");
+                }
+            }.start();
+        }
+        
+    }
+}
